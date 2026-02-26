f10ee9225bc538e45bddf7379d1fb188a6a16c7c
Amina <minassouane@gmail.com>
2026-02-26 16:58:58 +0100

    Add constraints, defaults, and correct drop order

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,30 +2,23 @@ const { Client } = require("pg");
 
 // CREATE TABLES
 const createUsers =
-  "CREATE TABLE users (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, firstname VARCHAR (255), lastname VARCHAR (255), username VARCHAR (255), password TEXT, is_member BOOLEAN DEFAULT FALSE, admin BOOLEAN DEFAULT FALSE)";
+  "CREATE TABLE users (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, firstname VARCHAR (255), lastname VARCHAR (255), username VARCHAR (255) UNIQUE NOT NULL, password TEXT NOT NULL, is_member BOOLEAN DEFAULT FALSE, admin BOOLEAN DEFAULT FALSE)";
 const createMessages =
-  "CREATE TABLE messages (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, user_id INTEGER, title VARCHAR (255), timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, text TEXT, FOREIGN KEY (user_id) REFERENCES users(id) )";
-
-// POPULATE TABLES
-// const populateUsers = "INSERT INTO users (id, first name, ";
-// const populateMessages;
+  "CREATE TABLE messages (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, user_id INTEGER NOT NULL, title VARCHAR (255) NOT NULL, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, text TEXT NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id) )";
 
 async function main() {
   const client = new Client({
-    connectionString: "postgresql://aminaassouane:null@localhost:5432/members",
+    connectionString: "postgresql://aminaassouane@localhost:5432/members",
   });
   try {
     await client.connect();
     console.log("Connected to database.");
-    await client.query("DROP TABLE IF EXISTS users;");
     await client.query("DROP TABLE IF EXISTS messages;");
+    await client.query("DROP TABLE IF EXISTS users;");
     console.log("Tables reset.");
     await client.query(createUsers);
     await client.query(createMessages);
     console.log("Tables created.");
-    await client.query(populateUsers);
-    await client.query(populateMessages);
-    console.log("Tables populated.");
   } catch (error) {
     console.error("Error occured : ", error);
   } finally {
