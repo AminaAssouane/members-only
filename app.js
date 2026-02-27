a5a0c1b2a0803a0adf6e6243befd0a77a246cced
Amina <minassouane@gmail.com>
2026-02-27 01:51:07 +0100

    chore: configure ejs

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const membersRouter = require("./routes/membersRouter");
+const path = require("node:path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.use("/", membersRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, (error) => {
+app.listen(PORT, (error) => {
   if (error) throw error;
   console.log(`Listening on port : ${PORT}`);
 });
