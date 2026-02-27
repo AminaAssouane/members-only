9281f868ffe6b190ebb867a2b151c9a220c3ae5b
Amina <minassouane@gmail.com>
2026-02-27 02:35:45 +0100

    feat: add urlencoded body parser

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("node:path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", membersRouter);
 
 const PORT = process.env.PORT || 3000;
