71cd459130982e9c0ae9440df78e63747e5ba6db
Amina <minassouane@gmail.com>
2026-02-27 03:13:30 +0100

    feat(db): add user creation query

---
+++ db/queries.js
@@ -0,0 +1,8 @@
+const pool = require("./pool");
+
+async function signUpPost(firstName, lastName, username, password) {
+  await pool.query(
+    "INSERT INTO users (firstname, lastname, username, password) VALUES ($1, $2, $3, $4)",
+    [firstName, lastName, username, password],
+  );
+}
